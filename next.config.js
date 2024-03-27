2b97b329f5b64acac6a48f3cef7f1f7db3b99747
Marlon Rodriguez Rivera <110213632+marlon7piri@users.noreply.github.com>
2024-03-27 08:52:26 -0500

    adding the exclude node modules 23

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    // Important: return the modified config
+    return config
+  },
+}
 
 module.exports = nextConfig
