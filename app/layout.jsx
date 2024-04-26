08f5a5aa834e494fa485d7fadcba923a475c0c4c
Marlon Rodriguez Rivera <110213632+marlon7piri@users.noreply.github.com>
2024-04-26 14:51:25 -0500

    add new config again 6

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,15 @@
-import { Poppins } from "next/font/google";
+/* import { Poppins } from "next/font/google"; */
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
 import { ClientProvider } from "./context/ClientProvider";
 import Footer from "./components/Footer";
-
+/* 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "500", "700", "900"],
 });
-
+ */
 export const metadata = {
   title: "Al-Alma Inventario",
   description: "Una aplicacion para tener control de inventario",
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <ClientProvider>
-        <body className={poppins.className}>
+        <body /* className={poppins.className} */>
           {children}
          
           <Footer />
