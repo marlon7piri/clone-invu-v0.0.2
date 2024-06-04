f4d9ca9498e4161e77b4f20b819408e03a468f26
marlon7piri <marlon7piri@gmail.com>
2024-06-04 16:40:49 -0500

    feature: changed the url web

--- app/libs/UrlWeb.js
+++ app/libs/UrlWeb.js
@@ -1,6 +1,6 @@
-const url = "https://clone-invu-app.vercel.app/api";
+const url = "https://clone-invu-v0-0-2.vercel.app/api";
 const url2 = "http://localhost:3000/api";
 
-export const UrlWeb = url2;
+export const UrlWeb = url;
 export const urlproveedores =
   "https://inventario-barra-backend.vercel.app/api/proveedores";
